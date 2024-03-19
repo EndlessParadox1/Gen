0993847c5ca58122112f55960508ded761fe9d06
EndlessParadox1 <endlessparadox250@gmail.com>
2024-03-19 11:32:04 +0800

    v0.2.0

--- recovery.go
+++ recovery.go
@@ -4,18 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"runtime"
+	"strings"
 )
 
 func trace(message string) string {
-	return ""
-} // TODO
+	var pcs [32]uintptr
+	n := runtime.Callers(3, pcs[:])
+	var s strings.Builder
+	s.WriteString(message + "\nTraceback:\n")
+	for _, pc := range pcs[:n] {
+		fn := runtime.FuncForPC(pc)
+		file, line := fn.FileLine(pc)
+		s.WriteString(fmt.Sprintf("\t%s: %d\n", file, line))
+	}
+	return s.String()
+}
 
 func Recovery() HandlerFunc {
 	return func(c *Context) {
 		defer func() {
 			if err := recover(); err != nil {
 				message := fmt.Sprintf("%s", err)
-				log.Printf("%s\n\n", trace(message))
+				log.Printf("%s\n", trace(message))
 				c.String(http.StatusInternalServerError, "Internal Server Error")
 			}
 		}()
