d35ecca8f7fbfc90d45faca09c2707b4a1a14318
EndlessParadox1 <endlessparadox250@gmail.com>
2024-03-19 12:38:52 +0800

    v0.2.3

--- logger.go
+++ logger.go
@@ -8,7 +8,6 @@ import (
 // Logger middleware that top group use
 func Logger() HandlerFunc {
 	return func(c *Context) {
-		log.SetPrefix("[GEN] ")
 		t := time.Now()
 		c.Next()
 		log.Printf("| %d | %13v | %15s | %-7s  %s\n", c.StatusCode, time.Since(t), c.RemoteIP(), c.Method, c.Path)
